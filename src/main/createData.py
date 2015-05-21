794681e5edaeeaedd74106b58964aa498aa5ed24
Amuerte <ben.amuerte@gmail.com>
2015-05-21 09:17:30 +0200

    the day he learned that walk() recurse

--- src/main/createData.py
+++ src/main/createData.py
@@ -6,8 +6,13 @@ imgDir = "resources/images/"
 for dirname in os.listdir(imgDir):
     for filename in os.listdir(imgDir + dirname):
         print filename
-"""
 
+{'pictures': {'pictures': ['pic1', 'pic2'], 'baseDir': 'toto', 'id': 'test'}}
+"""
+dataPics = {}
 for (dirpath, dirnames, filenames) in os.walk(imgDir):
-    for filename in filenames:
-        print dirpath + '/' + filename
+    for directory in dirnames:
+        dataPics[directory] = {'id':directory, 'baseDir':dirpath, 'pictures' : filenames}
+    #for path in dirpath:
+
+print dataPics

c119e6325f4deae5ac73a8b17f1a2a7fcb51c43d
Amuerte <ben.amuerte@gmail.com>
2015-05-22 02:51:53 +0200

    add some comprehension to the data script

--- src/main/createData.py
+++ src/main/createData.py
@@ -1,18 +1,20 @@
 import os
 
-imgDir = "resources/images/"
+rootDir = "resources/images/"
 
-"""
-for dirname in os.listdir(imgDir):
-    for filename in os.listdir(imgDir + dirname):
-        print filename
-
-{'pictures': {'pictures': ['pic1', 'pic2'], 'baseDir': 'toto', 'id': 'test'}}
-"""
 dataPics = {}
-for (dirpath, dirnames, filenames) in os.walk(imgDir):
-    for directory in dirnames:
-        dataPics[directory] = {'id':directory, 'baseDir':dirpath, 'pictures' : filenames}
-    #for path in dirpath:
+
+for (dirpath, dirnames, filenames) in os.walk(rootDir):
+
+    # Create entries for all directories of rootDir
+    if dirpath == rootDir:
+        dataPics = {x: {'id':x, 'baseDir':None, 'pictures':None} for x in dirnames}
+
+    # Add files to corresponding entry
+    else:
+        path = dirpath.replace(rootDir, '')
+        if not path.endswith('thumbs'):
+            dataPics[path]['pictures'] = filenames
+            dataPics[path]['baseDir'] = dirpath
 
 print dataPics
